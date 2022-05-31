012bfabb63fd9614d60f2c2b79792891f2689c39
Hammad Amjad <hammadamjadali30@gmail.com>
2022-05-31 13:13:38 +0500

    geo map commit 2

--- GeoMap/App.js
+++ GeoMap/App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import MapView, {
+  Callout,
+  Circle,
+  Marker,
+  Polyline,
+  PROVIDER_GOOGLE,
+} from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 
 export default function App() {
+  const [latitude, setLat] = useState(33.6844);
+  const [longitude, setLong] = useState(73.0479);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: 33.6844,
+          longitude: 73.0479,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        provider={PROVIDER_GOOGLE}
+      >
+        <Marker
+          draggable
+          coordinate={{ latitude: latitude, longitude: longitude }}
+          title="My Pin Title"
+          description="My Pin Description"
+          onDrag={(e) => {
+            setLat(e.nativeEvent.coordinate.latitude);
+            setLong(e.nativeEvent.coordinate.longitude);
+          }}
+        >
+          <Callout>
+            <Text>This is Callout</Text>
+          </Callout>
+        </Marker>
+        <Circle
+          center={{ latitude: latitude, longitude: longitude }}
+          radius={500}
+        ></Circle>
+      </MapView>
     </View>
   );
 }
@@ -13,8 +50,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
